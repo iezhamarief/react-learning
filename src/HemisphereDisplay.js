60235d30c9da2a8825694c8e3ab5fb55f6732eaf
Iezham Arief <102403018+iezhamarief@users.noreply.github.com>
2022-04-13 12:28:59 +0800

    13 Apr class

---
+++ src/HemisphereDisplay.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HemisphereDisplay = () => {
+
+    return(
+        <div>
+            Hey you are in northern hemisphere
+        </div>
+    )
+}
+
+export default HemisphereDisplay;
